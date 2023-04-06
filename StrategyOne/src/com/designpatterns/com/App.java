9b0e80ff9315f87f4c86d35ced4e8b3cd73b9752
albertofullstack <alberto.fullstack@gmail.com>
2023-04-06 19:56:44 +0200

    Add main class.

--- StrategyOne/src/com/designpatterns/com/App.java
+++ StrategyOne/src/com/designpatterns/com/App.java
@@ -1,7 +1,26 @@
 package com.designpatterns.com;
 
+import com.designpatterns.com.controller.ScoreBoard;
+import com.designpatterns.com.model.Balloon;
+import com.designpatterns.com.model.Clown;
+import com.designpatterns.com.model.SquareBalloon;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+
+  public static void main(String[] args) {
+
+    ScoreBoard scoreBoard = new ScoreBoard();
+
+    System.out.print("Balloon Tap Score: ");
+    scoreBoard.algorithmBase = new Balloon();
+    scoreBoard.showScore(10, 5);
+
+    System.out.print("Clown Tap Score: ");
+    scoreBoard.algorithmBase = new Clown();
+    scoreBoard.showScore(10, 5);
+
+    System.out.print("Square Balloon Score: ");
+    scoreBoard.algorithmBase = new SquareBalloon();
+    scoreBoard.showScore(10, 5);
+  }
 }
