3a007bd0c668ec1a2bcd1e2c5056f6115d1483de
albertofullstack <alberto.fullstack@gmail.com>
2024-03-02 14:07:20 +0100

    Order more pizzas for test.

--- Examples/Factory/PizzaFactoryMethod/src/dessignpattern/PizzaStoreTest.java
+++ Examples/Factory/PizzaFactoryMethod/src/dessignpattern/PizzaStoreTest.java
@@ -10,17 +10,25 @@ public class PizzaStoreTest {
   public static void main(String[] args) {
 
     PizzaStore nyPizzaStore = new NYPizzaStore();
+    System.out.println("Ordering NY Cheese Pizza");
     nyPizzaStore.orderPizza("cheese");
-    System.out.println("NY Pizza cheese - Order delivered");
+    System.out.println("NY Cheese Pizza - Order delivered");
 
+    System.out.println();
+    System.out.println("Ordering NY Veggie Pizza");
     nyPizzaStore.orderPizza("veggie");
-    System.out.println("NY Pizza cheese - Order delivered");
+    System.out.println("NY Veggie Pizza - Order delivered");
 
     System.out.println();
 
     PizzaStore chicagoPizzaStore = new ChicagoPizzaStore();
+    System.out.println("Ordering Chicago Cheese Pizza");
     chicagoPizzaStore.orderPizza("cheese");
+    System.out.println("Chicago Cheese Pizza - Order delivered");
+    System.out.println();
+
+    System.out.println("Ordering Chicago Clam Pizza");
     chicagoPizzaStore.orderPizza("clam");
-    System.out.println("Chicago Pizza cheese - Order delivered");
+    System.out.println("Chicago Clam Pizza - Order delivered");
   }
 }
