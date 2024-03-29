80b7b74cbbc7eeed50b85f92b068b70ad36299c5
albertofullstack <alberto.fullstack@gmail.com>
2024-03-29 13:04:58 +0100

    Implements Ceiling fan undo.

---
+++ Examples/Command/Undo/src/com/dessignpattern/command/implementations/CeilingFanMediumCommand.java
@@ -0,0 +1,36 @@
+package com.dessignpattern.command.implementations;
+
+import com.dessignpattern.command.interfaces.Command;
+import com.dessignpattern.command.vendor.CeilingFan;
+
+public class CeilingFanMediumCommand implements Command {
+
+  private CeilingFan ceilingFan;
+
+  private int prevSpeed;
+
+  public CeilingFanMediumCommand(CeilingFan ceilingFan) {
+    this.ceilingFan = ceilingFan;
+  }
+
+  @Override
+  public void execute() {
+    prevSpeed = this.ceilingFan.getSpeed();
+    this.ceilingFan.medium();
+  }
+
+  @Override
+  public void undo() {
+    switch (prevSpeed) {
+      case CeilingFan.HIGH:
+        this.ceilingFan.high();
+        break;
+      case CeilingFan.MEDIUM:
+        this.ceilingFan.medium();
+        break;
+      case CeilingFan.LOW:
+        this.ceilingFan.low();
+        break;
+    }
+  }
+}
