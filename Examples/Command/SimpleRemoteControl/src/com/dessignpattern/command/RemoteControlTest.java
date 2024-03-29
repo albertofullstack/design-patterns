f728231280d9c441784312467c5eb9e916a575ea
albertofullstack <alberto.fullstack@gmail.com>
2024-03-29 09:07:16 +0100

    Add main.

---
+++ Examples/Command/SimpleRemoteControl/src/com/dessignpattern/command/RemoteControlTest.java
@@ -0,0 +1,19 @@
+package com.dessignpattern.command;
+
+import com.dessignpattern.command.implementations.LightOnCommand;
+import com.dessignpattern.command.implementations.SimpleRemoteControl;
+import com.dessignpattern.command.interfaces.Command;
+import com.dessignpattern.command.vendor.Light;
+
+public class RemoteControlTest {
+
+  public static void main(String[] args) {
+
+    Light light = new Light();
+    Command command = new LightOnCommand(light);
+
+    SimpleRemoteControl simpleRemoteControl = new SimpleRemoteControl();
+    simpleRemoteControl.setCommand(command);
+    simpleRemoteControl.pressButton();
+  }
+}
