cf7a3fbacd7b4c6a4eafc1898056717e851b451e
albertofullstack <alberto.fullstack@gmail.com>
2024-03-31 11:42:26 +0200

    Implements test class.

--- Examples/Template/BaristaWithHook/src/com/dessignpattern/templatemethod/Main.java
+++ Examples/Template/BaristaWithHook/src/com/dessignpattern/templatemethod/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
   public static void main(String[] args) {
 
-    Tea tea = new Tea();
-    Coffee coffee = new Coffee();
+    TeaWithHook tea = new TeaWithHook();
+    CoffeeWithHook coffee = new CoffeeWithHook();
     System.out.println("Making tea...");
     tea.prepareRecipe();
-    System.out.println("Making coffee...");
+    System.out.println("\nMaking coffee...");
     coffee.prepareRecipe();
   }
 }
