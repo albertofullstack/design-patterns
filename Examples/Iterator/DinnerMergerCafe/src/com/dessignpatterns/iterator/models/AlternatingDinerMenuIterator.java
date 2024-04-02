d85b304b2ac6b5644681e23c09a8d401df3bfc96
albertofullstack <alberto.fullstack@gmail.com>
2024-04-02 11:50:17 +0200

    Implements AlternatingDinerMenuIterator.

---
+++ Examples/Iterator/DinnerMergerCafe/src/com/dessignpatterns/iterator/models/AlternatingDinerMenuIterator.java
@@ -0,0 +1,32 @@
+package com.dessignpatterns.iterator.models;
+
+import java.util.Calendar;
+import java.util.Iterator;
+
+public class AlternatingDinerMenuIterator implements Iterator {
+
+  MenuItem[] items;
+  int position;
+
+  public AlternatingDinerMenuIterator(MenuItem[] items, int position) {
+    this.items = items;
+    this.position = Calendar.DAY_OF_WEEK % 2;
+  }
+
+  @Override
+  public boolean hasNext() {
+    return position < items.length && items[position] != null;
+  }
+
+  @Override
+  public Object next() {
+    MenuItem menuItem = items[position];
+    position += 2;
+    return menuItem;
+  }
+
+  @Override
+  public void remove() {
+    throw new UnsupportedOperationException("Alternating Diner Menu Iterator does not support remove()");
+  }
+}
