af693fde41d0235fd64dee9113cb73e38507a489
albertofullstack <alberto.fullstack@gmail.com>
2024-04-06 12:57:41 +0200

    Add composite iterator base project.

---
+++ Examples/Composite/CompositeIterator/src/com/dessignpatterns/composite/models/Waitress.java
@@ -0,0 +1,34 @@
+package com.dessignpatterns.composite.models;
+
+import com.dessignpatterns.composite.interfaces.MenuComponent;
+
+public class Waitress {
+
+  MenuComponent menus;
+
+  public Waitress(MenuComponent menus) {
+    this.menus = menus;
+  }
+
+  public void printMenu() {
+
+    menus.print();
+  }
+
+  public void printBreakfastMenu() {
+
+  }
+
+  public void printLunchMenu() {
+
+  }
+
+  public void printVegetarianMenu() {
+
+  }
+
+  public boolean isItemVegetarian(String name) {
+
+    return false;
+  }
+}
