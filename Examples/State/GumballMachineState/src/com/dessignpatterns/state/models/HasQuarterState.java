e847b2ecbb335fd8db38a1adfd4e34d8f0e61b1d
albertofullstack <alberto.fullstack@gmail.com>
2024-04-07 14:53:16 +0200

    Implement winner state.

--- Examples/State/GumballMachineState/src/com/dessignpatterns/state/models/HasQuarterState.java
+++ Examples/State/GumballMachineState/src/com/dessignpatterns/state/models/HasQuarterState.java
@@ -1,10 +1,12 @@
 package com.dessignpatterns.state.models;
 
 import com.dessignpatterns.state.interfaces.State;
+import java.util.Random;
 
 public class HasQuarterState implements State {
 
   private GumballMachine gumballMachine;
+  Random randomWinner = new Random(System.currentTimeMillis());
 
   public HasQuarterState(GumballMachine gumballMachine) {
     this.gumballMachine = gumballMachine;
@@ -21,6 +23,12 @@ public class HasQuarterState implements State {
 
   public void turnCrank() {
     System.out.println("You turned...");
+    int winner = randomWinner.nextInt(10);
+    if ((winner == 0) && gumballMachine.getCount() > 1) {
+      gumballMachine.setState(gumballMachine.getWinnerState());
+    } else {
+      gumballMachine.setState(gumballMachine.getSoldState());
+    }
     gumballMachine.setState(gumballMachine.getSoldState());
   }
 
