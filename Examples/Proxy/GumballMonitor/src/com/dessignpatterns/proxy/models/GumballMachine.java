b71385c955c06a10e9270b931ec8e2889c01422a
albertofullstack <alberto.fullstack@gmail.com>
2024-04-14 12:21:28 +0200

    Add location to GumballMachine.

--- Examples/Proxy/GumballMonitor/src/com/dessignpatterns/proxy/models/GumballMachine.java
+++ Examples/Proxy/GumballMonitor/src/com/dessignpatterns/proxy/models/GumballMachine.java
@@ -11,15 +11,18 @@ public class GumballMachine {
   State soldState;
   State winnerState;
 
+  String location;
+
   State state = new SoldOutState(this);
   int count = 0;
 
-  public GumballMachine(int count) {
+  public GumballMachine(String location, int count) {
     soldOutState = new SoldOutState(this);
     noQuarterState = new NoQuarterState(this);
     hasQuarterState = new HasQuarterState(this);
     soldState = new SoldState(this);
     winnerState = new WinnerState(this);
+    this.location = location;
 
     this.count = count;
     if (count > 0) {
@@ -89,5 +92,9 @@ public class GumballMachine {
 
   public int getCount() {
     return count;
+  }
+
+  public String getLocation() {
+    return location;
   }
 }
