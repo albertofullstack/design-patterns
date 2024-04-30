53f2c46ce68649b534bd0cd08061288eb53aaf5d
albertofullstack <alberto.fullstack@gmail.com>
2024-04-30 14:45:53 +0200

    Modify MyRemoteClient.

--- Examples/Proxy/HelloRemoteClient/src/MyRemoteClient.java
+++ Examples/Proxy/HelloRemoteClient/src/MyRemoteClient.java
@@ -4,7 +4,7 @@ public class MyRemoteClient {
 
   public static void main(String[] args) {
     try {
-      MyRemote stub = (MyRemote) Naming.lookup("rmi://localhost/RemoteHello");
+      MyRemote stub = (MyRemote) Naming.lookup("rmi://127.0.0.1/RemoteHello");
       String s = stub.sayHello();
       System.out.println(s);
     } catch (Exception e) {
