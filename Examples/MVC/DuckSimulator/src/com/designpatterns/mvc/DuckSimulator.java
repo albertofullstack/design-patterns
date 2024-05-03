71ed4facfc7334aba89f991c698f2c5218e6dfb9
albertofullstack <alberto.fullstack@gmail.com>
2024-05-03 10:01:45 +0200

    Decorates Ducks to count number of quacks.

--- Examples/MVC/DuckSimulator/src/com/designpatterns/mvc/DuckSimulator.java
+++ Examples/MVC/DuckSimulator/src/com/designpatterns/mvc/DuckSimulator.java
@@ -8,10 +8,10 @@ public class DuckSimulator {
   }
 
   void simulate() {
-    Quackable mallardDuck = new MallardDuck();
-    Quackable redheadDuck = new RedheadDuck();
-    Quackable duckCall = new DuckCall();
-    Quackable rubberDuck = new RubberDuck();
+    Quackable mallardDuck = new QuackCounter(new MallardDuck());
+    Quackable redheadDuck = new QuackCounter(new RedheadDuck());
+    Quackable duckCall = new QuackCounter(new DuckCall());
+    Quackable rubberDuck = new QuackCounter(new RubberDuck());
     Quackable gooseDuck = new GooseAdapter(new Goose());
 
     System.out.println("\nDuck Simulator");
@@ -21,6 +21,8 @@ public class DuckSimulator {
     simulate(duckCall);
     simulate(rubberDuck);
     simulate(gooseDuck);
+
+    System.out.println("The ducks quacked " + QuackCounter.numberOfQuacks + " times");
   }
 
   void simulate(Quackable duck) {
