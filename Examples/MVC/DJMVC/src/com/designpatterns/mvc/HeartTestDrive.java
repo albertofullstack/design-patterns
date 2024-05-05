df50bd2b0d514c8625d51d4da20ca0a1637812ee
albertofullstack <alberto.fullstack@gmail.com>
2024-05-05 13:29:37 +0200

    Improves HeartTestDrive code.

--- Examples/MVC/DJMVC/src/com/designpatterns/mvc/HeartTestDrive.java
+++ Examples/MVC/DJMVC/src/com/designpatterns/mvc/HeartTestDrive.java
@@ -3,6 +3,6 @@ package com.designpatterns.mvc;
 public class HeartTestDrive {
   public static void main (String[] args) {
     HeartModel heartModel = new HeartModel();
-    ControllerInterface model = new HeartController(heartModel);
+    new HeartController(heartModel);
   }
 }
