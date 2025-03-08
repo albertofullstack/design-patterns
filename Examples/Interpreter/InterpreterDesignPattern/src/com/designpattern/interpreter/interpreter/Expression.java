62d53514832f6d2020e4cf2d8d6fde013f5a0afc
albertofullstack <alberto.fullstack@gmail.com>
2025-03-08 11:02:53 +0100

    Add interpreter pattern.

---
+++ Examples/Interpreter/InterpreterDesignPattern/src/com/designpattern/interpreter/interpreter/Expression.java
@@ -0,0 +1,6 @@
+package com.designpattern.interpreter.interpreter;
+
+public interface Expression {
+
+  String interpreter(InterpreterContext context);
+}
---
+++ Examples/Interpreter/InterpreterDesignPattern/src/com/designpattern/interpreter/interpreter/IntToBinaryExpression.java
@@ -0,0 +1,14 @@
+package com.designpattern.interpreter.interpreter;
+
+public class IntToBinaryExpression implements Expression {
+
+  private int i;
+  public IntToBinaryExpression(int i) {
+    this.i = i;
+  }
+
+  @Override
+  public String interpreter(InterpreterContext context) {
+    return context.getBinaryFormat(i);
+  }
+}
